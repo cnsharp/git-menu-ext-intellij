cdf330eb4c982b46c57a7d3e3e5d8de77c255f2d
Bob Chin <cnsharp@yeah.net>
2026-06-08 19:35:14 +0800

    init

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "1.9.25"
+    id("org.jetbrains.intellij") version "1.17.4"
+}
+
+group = "com.cnsharp"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+intellij {
+    version.set("2024.1")
+    type.set("IC")
+    plugins.set(listOf("git4idea"))
+}
+
+tasks {
+    withType<JavaCompile> {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "17"
+    }
+    patchPluginXml {
+        sinceBuild.set("241")
+        untilBuild.set("251.*")
+    }
+}
